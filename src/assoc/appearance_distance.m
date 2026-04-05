7588e6222af8ce2ed34c46d991930a651d63fd1b
samuelecivale <samuele.civale@gmail.com>
2026-04-05 23:27:01 +0200

    prune unused scripts and legacy helper files

--- src/assoc/appearance_distance.m
+++
@@ -1,7 +0,0 @@
-function d = appearance_distance(a, b)
-    assert(isvector(a) && isvector(b), 'appearance_distance expects two vectors');
-    assert(numel(a) == numel(b), 'Descriptor sizes must match');
-
-    diff = a(:) - b(:);
-    d = norm(diff);
-end
--- src/geometry/t2v.m
+++
@@ -1,9 +0,0 @@
-function v = t2v(T)
-    assert(all(size(T) == [3, 3]), 't2v expects a 3x3 matrix');
-
-    x = T(1, 3);
-    y = T(2, 3);
-    theta = atan2(T(2, 1), T(1, 1));
-
-    v = [x; y; theta];
-end
